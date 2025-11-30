eff18d66d67efcc370a48007593f1555e57c8090
Kaynart <kaynancabj4b@gmail.com>
2025-11-30 20:16:43 -0300

    chore: remove linhas comentadas que podem ser dispensadas já

--- rotas/rotinas.py
+++ rotas/rotinas.py
@@ -61,9 +61,6 @@ async def listar_rotinas_usuario_logado(
     """Lista todas as rotinas criadas pelo usuário autenticado"""
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        # id_usuario = int(payload.get("sub"))
-        # if id_usuario is None:
-        #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido.")
 
         sub_claim = payload.get("sub") # primeiro isola essa captura, porque pode ser none, então não pode tentar converter pra int agora
         if sub_claim is None: # confere se é none
